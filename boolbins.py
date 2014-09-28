2d7f435c4406bf7516c77e14fc0d8a0b1f250b45
Andrew Carman <carmandrew@gmail.com>
2014-09-27 20:48:44 -0700

    Updates to flip the output axis, add a limit option, and handle tsv input

--- boolbins.py
+++ boolbins.py
@@ -1,34 +1,63 @@
+#!/usr/bin/env python
+
 import argparse
 import os.path
 import csv
 import logging
 import collections
+import contextlib
 
 
 IGNORED_INITIAL_COLUMN_COUNT = 2
 
 
-def get_frequencies_for_file(fileName, thresholds):
-    logging.debug("\n")
-    logging.debug("Processing file %s", fileName)
-    reader = csv.reader(open(fileName, 'rU'))
-    header = reader.next()
-    frequencies = collections.defaultdict(float)
-    total_cells = 0
+@contextlib.contextmanager
+def safe_open_csv_file(file_name, minimum_columns=1):
+    """Try to open the file as a csv. If there's only one column in the first row, then try to open as a tsv.
+    It that still only has one column, throw an error."""
+    with open(file_name, 'rU') as f:
+        reader = csv.reader(f)
+        if len(reader.next()) > minimum_columns:
+            f.seek(0)
+            yield reader
+            return
+        f.seek(0)
+        reader = csv.reader(f, delimiter="\t")
+        if len(reader.next()) > minimum_columns:
+            f.seek(0)
+            yield reader
+            return
+    raise Exception("Error opening csv file %s, neither comma nor tab separated" % file_name)
 
-    for row in reader:
-        total_cells += 1
-        headers_above_threshold = set()
-        for i in xrange(IGNORED_INITIAL_COLUMN_COUNT, len(header)):
-            if header[i] in thresholds and float(row[i]) >= thresholds[header[i]]:
-                headers_above_threshold.add(header[i])
-        if headers_above_threshold:
-            frequencies[frozenset(headers_above_threshold)] += 1
+
+def get_frequencies_for_file(file_name, thresholds, limit):
+    logging.debug("\n")
+    logging.info("Processing file %s", file_name)
+
+    with safe_open_csv_file(file_name) as reader:
+        header = reader.next()
+        frequencies = collections.defaultdict(float)
+        total_cells = processed_cells = 0
+
+        for row in reader:
+            total_cells += 1
+            if limit == 0 or total_cells <= limit:
+                processed_cells += 1
+                headers_above_threshold = set()
+                for i in xrange(IGNORED_INITIAL_COLUMN_COUNT, len(header)):
+                    if header[i] in thresholds and float(row[i]) >= thresholds[header[i]]:
+                        headers_above_threshold.add(header[i])
+                if headers_above_threshold:
+                    frequencies[frozenset(headers_above_threshold)] += 1
 
     for header, count in frequencies.iteritems():
-        frequencies[header] = count * 1.0 / total_cells
+        frequencies[header] = count * 1.0 / processed_cells
 
-    logging.debug("File had: %s cells with overall frequencies:" % total_cells)
+    if limit == 0:
+        logging.info("File had: %s cells" % total_cells)
+    else:
+        logging.info("File had: %s cells. Limited to first %s" % (total_cells, limit))
+    logging.debug("Antibody frequencies:")
     for headers, freq in frequencies.iteritems():
         logging.debug("    %s: %s" % (headers, freq))
 
@@ -36,11 +65,12 @@ def get_frequencies_for_file(fileName, thresholds):
 
 
 def make_pretty_name_for_bin_combo(combo):
-    return " ".join(sorted(list(combo)))
+    # each item in combo is like set("(Cd110)Dd: HLADR", "(Ce140)Dd: beads")
+    return " ".join(sorted([i.split(" ")[-1] for i in combo]))
 
 
-def process_files(file_names, thresholds, output_file_name):
-    frequencies_by_file = {f: get_frequencies_for_file(f, thresholds) for f in file_names}
+def process_files(file_names, thresholds, output_file_name, limit):
+    frequencies_by_file = {f: get_frequencies_for_file(f, thresholds, limit) for f in file_names}
 
     # This is the set of all possible antibody combinations that are above the threshold on a cell. ie. a set of sets of headers
     all_possible_bin_combos = set()
@@ -52,12 +82,25 @@ def process_files(file_names, thresholds, output_file_name):
 
     with open(output_file_name, 'w') as output:
         w = csv.writer(output)
-        w.writerow(["Filename"] + [make_pretty_name_for_bin_combo(i) for i in all_possible_bin_combos])
+        files = frequencies_by_file.keys()
+        files.sort()
 
-        for f, frequencies in frequencies_by_file.iteritems():
-            row = [f]
-            for combo in all_possible_bin_combos:
-                row.append(frequencies.get(combo, 0))
+        # Write out the first header row
+        w.writerow([""] + files)
+
+        # Write out the null row (that is, what % of cells have every marker below the thresholds)
+        null_row = ["[]"]
+        for f in files:
+            freq = frequencies_by_file[f]
+            total_freq_for_file = sum([freq.get(i, 0) for i in all_possible_bin_combos])
+            null_row.append(1 - total_freq_for_file)
+        w.writerow(null_row)
+
+        # Write out a row for each other combo of antibodies
+        for combo in all_possible_bin_combos:
+            row = [make_pretty_name_for_bin_combo(combo)]
+            for f in files:
+                row.append(frequencies_by_file[f].get(combo, 0))
             w.writerow(row)
 
 
@@ -71,17 +114,18 @@ def discover_all_files(files_or_directories):
             all_valid_files.append(f)
         else:
             logging.debug("Ignoring %s" % f)
-    logging.debug("Got %s files to process:" % len(all_valid_files))
+    logging.info("Got %s files to process:" % len(all_valid_files))
     for f in all_valid_files:
         logging.debug("    %s" % f)
     return all_valid_files
 
 
-def get_thresholds_from_file(f):
-    reader = csv.reader(f)
-    headers = reader.next()
-    thresholds = reader.next()
-    to_include = reader.next()
+def get_thresholds_from_file(file_name):
+    with safe_open_csv_file(file_name) as reader:
+        headers = reader.next()
+        thresholds = reader.next()
+        to_include = reader.next()
+
     if len(headers) != len(thresholds) or len(headers) != len(to_include):
         raise Exception("Invalid threshold file, the headers (%s), thresholds (%s), and inclusions (%s) row lengths don't match" % (
             len(headers), len(thresholds), len(to_include)))
@@ -90,24 +134,27 @@ def get_thresholds_from_file(f):
     for i in xrange(IGNORED_INITIAL_COLUMN_COUNT, len(headers)):
         if to_include[i].lower().strip() in ["true", "yes", "1", "t"]:
             thresholds_per_header[headers[i]] = float(thresholds[i])
+
+    logging.info("Loaded %s thresholds" % len(thresholds_per_header))
     return thresholds_per_header
 
 
-def run(threshold_file, file_names, output_file_name):
-    thresholds = get_thresholds_from_file(threshold_file)
+def run(threshold_file_name, file_names, output_file_name, limit):
+    thresholds = get_thresholds_from_file(threshold_file_name)
     logging.debug("Got thresholds: %s" % thresholds)
-    process_files(discover_all_files(file_names), thresholds, output_file_name)
+    process_files(discover_all_files(file_names), thresholds, output_file_name, limit)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Performs boolean gating on flow cytometry data')
-    parser.add_argument('--thresholds', type=file, required=True, help='File with the thresholds to use for each antibody')
+    parser.add_argument('--thresholds', type=str, required=True, help='File with the thresholds to use for each antibody')
     parser.add_argument('--output', type=str, default="output.csv", help='File to output the data to')
     parser.add_argument('--debug', action="store_true", help='Show debug logging')
+    parser.add_argument('--limit', type=int, default=0, help='Process the first `limit` lines of each file, 0 (default) for all of them.')
     parser.add_argument('files', metavar='file', type=str, nargs='+', help='Files or directories to process')
 
     args = parser.parse_args()
-    log_level = logging.DEBUG if args.debug else logging.WARNING
+    log_level = logging.DEBUG if args.debug else logging.INFO
     logging.basicConfig(level=log_level)
 
-    run(args.thresholds, args.files, args.output)
+    run(args.thresholds, args.files, args.output, args.limit)
--- test.py
+++ test.py
@@ -1,22 +1,34 @@
+#!/usr/bin/env python
+
 import boolbins
 import unittest
 import os.path
 
 
 SIMPLE_OUT = """
-Filename,(Ce140)Dd: beads
-tests/first9.csv,0.2222222222222222
+,tests/first9.csv
+[],0.7777777777777778
+beads,0.2222222222222222
+"""
+SIMPLE_OUT_WITH_LIMIT = """
+,tests/first9.csv
+[],0.33333333333333337
+beads,0.6666666666666666
 """
 MULTIPLE_OUT = """
-Filename,(Cd110)Dd: HLADR,(Ce140)Dd: beads,(Cd110)Dd: HLADR (Ce140)Dd: beads
-tests/first9.csv,0.2222222222222222,0.1111111111111111,0.1111111111111111
+,tests/first9.csv
+[],0.5555555555555556
+HLADR,0.2222222222222222
+beads,0.1111111111111111
+HLADR beads,0.1111111111111111
 """
 MULTIPLE_FILES_OUT = """
-Filename,(Cd110)Dd: HLADR,(Ce140)Dd: beads,(Cd110)Dd: HLADR (Ce140)Dd: beads
-tests/first5.csv,0.2,0,0.2
-tests/first9.csv,0.2222222222222222,0.1111111111111111,0.1111111111111111
+,tests/first5.csv,tests/first9.csv
+[],0.6,0.5555555555555556
+HLADR,0.2,0.2222222222222222
+beads,0,0.1111111111111111
+HLADR beads,0.2,0.1111111111111111
 """
-
 
 class TestBasic(unittest.TestCase):
     output_file = "tests/output.csv"
@@ -31,13 +43,17 @@ class TestBasic(unittest.TestCase):
         self.cleanup_files()
 
     def test_one_threshold(self):
-        boolbins.run(open("tests/thresholds_simple.csv"), ["tests/first9.csv"], self.output_file)
+        boolbins.run("tests/thresholds_simple.csv", ["tests/first9.csv"], self.output_file, 0)
         self.assertEqual(open(self.output_file, "rU").read().strip(), SIMPLE_OUT.strip())
 
+    def test_one_threshold_with_limit(self):
+        boolbins.run("tests/thresholds_simple.csv", ["tests/first9.csv"], self.output_file, 3)
+        self.assertEqual(open(self.output_file, "rU").read().strip(), SIMPLE_OUT_WITH_LIMIT.strip())
+
     def test_multiple_thresholds(self):
-        boolbins.run(open("tests/thresholds_multiple.csv"), ["tests/first9.csv"], self.output_file)
+        boolbins.run("tests/thresholds_multiple.csv", ["tests/first9.csv"], self.output_file, 0)
         self.assertEqual(open(self.output_file, "rU").read().strip(), MULTIPLE_OUT.strip())
 
     def test_multiple_thresholds_multiple_files(self):
-        boolbins.run(open("tests/thresholds_multiple.csv"), ["tests/first9.csv", "tests/first5.csv"], self.output_file)
-        self.assertEqual(open(self.output_file, "rU").read().strip(), MULTIPLE_OUT.strip())
+        boolbins.run("tests/thresholds_multiple.csv", ["tests/first9.csv", "tests/first5.csv"], self.output_file, 0)
+        self.assertEqual(open(self.output_file, "rU").read().strip(), MULTIPLE_FILES_OUT.strip())
