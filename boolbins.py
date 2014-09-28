8df629c4d7ab9f896299e2c4c410262299e86169
Andrew Carman <carmandrew@gmail.com>
2014-09-27 22:55:10 -0700

    Better error handling, better null cell handling

--- boolbins.py
+++ boolbins.py
@@ -8,7 +8,10 @@ import collections
 import contextlib
 
 
-IGNORED_INITIAL_COLUMN_COUNT = 2
+IGNORED_INITIAL_COLUMNS = ["Time", "Cell_length"]
+
+
+class BoolBinsException(Exception): pass
 
 
 @contextlib.contextmanager
@@ -27,7 +30,7 @@ def safe_open_csv_file(file_name, minimum_columns=1):
             f.seek(0)
             yield reader
             return
-    raise Exception("Error opening csv file %s, neither comma nor tab separated" % file_name)
+    raise BoolBinsException("Error opening csv file %s, neither comma nor tab separated" % file_name)
 
 
 def get_frequencies_for_file(file_name, thresholds, limit):
@@ -44,11 +47,11 @@ def get_frequencies_for_file(file_name, thresholds, limit):
             if limit == 0 or total_cells <= limit:
                 processed_cells += 1
                 headers_above_threshold = set()
-                for i in xrange(IGNORED_INITIAL_COLUMN_COUNT, len(header)):
-                    if header[i] in thresholds and float(row[i]) >= thresholds[header[i]]:
+                for i in xrange(len(IGNORED_INITIAL_COLUMNS), len(header)):
+                    if header[i] in thresholds and float(row[i]) > thresholds[header[i]]:
                         headers_above_threshold.add(header[i])
-                if headers_above_threshold:
-                    frequencies[frozenset(headers_above_threshold)] += 1
+                # Note, the empty set represents null cells
+                frequencies[frozenset(headers_above_threshold)] += 1
 
     for header, count in frequencies.iteritems():
         frequencies[header] = count * 1.0 / processed_cells
@@ -66,18 +69,22 @@ def get_frequencies_for_file(file_name, thresholds, limit):
 
 def make_pretty_name_for_bin_combo(combo):
     # each item in combo is like set("(Cd110)Dd: HLADR", "(Ce140)Dd: beads")
+    if not combo:
+        # The null cell
+        return "[]"
     return " ".join(sorted([i.split(" ")[-1] for i in combo]))
 
 
 def process_files(file_names, thresholds, output_file_name, limit):
     frequencies_by_file = {f: get_frequencies_for_file(f, thresholds, limit) for f in file_names}
 
-    # This is the set of all possible antibody combinations that are above the threshold on a cell. ie. a set of sets of headers
+    # This is the set of all possible antibody combinations that are above the threshold on a cell.
+    # ie. a set of sets of headers
     all_possible_bin_combos = set()
     for _, frequencies in frequencies_by_file.iteritems():
         all_possible_bin_combos |= set(frequencies.keys())
     all_possible_bin_combos = list(all_possible_bin_combos)
-    all_possible_bin_combos.sort()
+    all_possible_bin_combos.sort(key=lambda x: (len(x), x))
     logging.debug("Got %s possible antibody combinations: %s" % (len(all_possible_bin_combos), all_possible_bin_combos))
 
     with open(output_file_name, 'w') as output:
@@ -88,14 +95,6 @@ def process_files(file_names, thresholds, output_file_name, limit):
         # Write out the first header row
         w.writerow([""] + files)
 
-        # Write out the null row (that is, what % of cells have every marker below the thresholds)
-        null_row = ["[]"]
-        for f in files:
-            freq = frequencies_by_file[f]
-            total_freq_for_file = sum([freq.get(i, 0) for i in all_possible_bin_combos])
-            null_row.append(1 - total_freq_for_file)
-        w.writerow(null_row)
-
         # Write out a row for each other combo of antibodies
         for combo in all_possible_bin_combos:
             row = [make_pretty_name_for_bin_combo(combo)]
@@ -104,16 +103,21 @@ def process_files(file_names, thresholds, output_file_name, limit):
             w.writerow(row)
 
 
-def discover_all_files(files_or_directories):
+def discover_all_files(headers, files_or_directories):
     all_valid_files = []
     while len(files_or_directories) > 0:
         f = files_or_directories.pop()
         if os.path.isdir(f):
             files_or_directories += [os.path.join(f, i) for i in os.listdir(f)]
         elif f.endswith(".txt") or f.endswith(".csv"):
-            all_valid_files.append(f)
+            with safe_open_csv_file(f) as r:
+                header_line = r.next()
+            if header_line == headers:
+                all_valid_files.append(f)
+            else:
+                logging.warning("Ignoring file %s, headers don't match the thresholds file" % f)
         else:
-            logging.debug("Ignoring %s" % f)
+            logging.warning("Ignoring %s, not a csv or txt file" % f)
     logging.info("Got %s files to process:" % len(all_valid_files))
     for f in all_valid_files:
         logging.debug("    %s" % f)
@@ -126,23 +130,26 @@ def get_thresholds_from_file(file_name):
         thresholds = reader.next()
         to_include = reader.next()
 
+    if headers[:len(IGNORED_INITIAL_COLUMNS)] != IGNORED_INITIAL_COLUMNS:
+        raise BoolBinsException("Badly formatted thresholds file. Needs initial headers: %s" % IGNORED_INITIAL_COLUMNS)
+
     if len(headers) != len(thresholds) or len(headers) != len(to_include):
-        raise Exception("Invalid threshold file, the headers (%s), thresholds (%s), and inclusions (%s) row lengths don't match" % (
+        raise BoolBinsException("Invalid threshold file, the headers (%s), thresholds (%s), and inclusions (%s) row lengths don't match" % (
             len(headers), len(thresholds), len(to_include)))
 
     thresholds_per_header = {}
-    for i in xrange(IGNORED_INITIAL_COLUMN_COUNT, len(headers)):
+    for i in xrange(len(IGNORED_INITIAL_COLUMNS), len(headers)):
         if to_include[i].lower().strip() in ["true", "yes", "1", "t"]:
             thresholds_per_header[headers[i]] = float(thresholds[i])
 
     logging.info("Loaded %s thresholds" % len(thresholds_per_header))
-    return thresholds_per_header
+    return headers, thresholds_per_header
 
 
 def run(threshold_file_name, file_names, output_file_name, limit):
-    thresholds = get_thresholds_from_file(threshold_file_name)
+    headers, thresholds = get_thresholds_from_file(threshold_file_name)
     logging.debug("Got thresholds: %s" % thresholds)
-    process_files(discover_all_files(file_names), thresholds, output_file_name, limit)
+    process_files(discover_all_files(headers, file_names), thresholds, output_file_name, limit)
 
 
 if __name__ == '__main__':
@@ -156,5 +163,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     log_level = logging.DEBUG if args.debug else logging.INFO
     logging.basicConfig(level=log_level)
-
-    run(args.thresholds, args.files, args.output, args.limit)
+    try:
+        run(args.thresholds, args.files, args.output, args.limit)
+    except BoolBinsException as e:
+        logging.error("%s" % e)
